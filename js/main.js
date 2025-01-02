5fce43a5dc0ad5b4dffd58f06f201e1139f4b513
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2025-01-02 22:26:16 +0500

    Блок преимущества и форму консультации

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 const iconMenu = document.querySelector("[data-js-header-burger-button]");
 const menuBody = document.querySelector("[data-js-menu-body]");
 const htmlElement = document.documentElement;
+let inputs = document.querySelectorAll('input[type="tel"]');
 
 function openMenu() {
-	if(iconMenu) {
-		iconMenu.addEventListener("click", function(event){
-			iconMenu.classList.toggle("is-active");
-			menuBody.classList.toggle("is-active");
-			htmlElement.classList.toggle("is-lock")
-		})
-	}
+  if (iconMenu) {
+    iconMenu.addEventListener("click", function (event) {
+      iconMenu.classList.toggle("is-active");
+      menuBody.classList.toggle("is-active");
+      htmlElement.classList.toggle("is-lock");
+    });
+  }
 }
 
 openMenu();
 
-
-
+var im = new Inputmask("+7 (999) 999-99-99");
+im.mask(inputs);
